b363c525a4c26ed6824d40e1a294c30e4eaa2be7
OSSAS\roder <roderickhalim@gmail.com>
2017-07-04 20:32:14 +0700

    seed user database & install jwt

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\Bar;
+use JWTAuth;
+use Auth;
+use Exception;
+
+use App\Http\Requests\StoreBar;
+
 
 class LoginController extends Controller
 {
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Database\Seeder;
 
-use App\User;
-
 class UserSeeder extends Seeder
 {
     /**
@@ -13,28 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-          User::Insert([
-            ['name' => 'Test',
-            'email' => 'email@gmail.com',
-            'password' => 'testes'],
-            ['name' => 'Test1',
-            'email' => 'email1@gmail.com',
-            'password' => 'testes'],
-            ['name' => 'Test2',
-            'email' => 'email2@gmail.com',
-            'password' => 'testes'],
-            ['name' => 'Test3',
-            'email' => 'email3@gmail.com',
-            'password' => 'testes'],
-            ['name' => 'Test4',
-            'email' => 'email4@gmail.com',
-            'password' => 'testes'],
-            ['name' => 'Test5',
-            'email' => 'email5@gmail.com',
-            'password' => 'testes'],
-            ['name' => 'Test6',
-            'email' => 'email6@gmail.com',
-            'password' => 'testes'],
-          ]);
+        factory(App\User::class,50)->create();
     }
 }
